7978c1fe92c3d9e591ff01f374ae5bf96715b664
noshi91 <noshita_y@outlook.jp>
2019-09-17 01:43:48 +0900

    add persistent_skew_binary_list

---
+++ lib/data_structure/sequence/persistent_skew_binary_list.hpp
@@ -0,0 +1,121 @@
+#ifndef NIMI_DATASTRUCTURE_SEQ_PSKEWBINLIST
+#define NIMI_DATASTRUCTURE_SEQ_PSKEWBINLIST
+
+#include <cstddef>
+#include <memory>
+#include <utility>
+
+namespace n91 {
+
+template <class T, class Size = std::size_t> class persistent_skew_binary_list {
+  class tree_type;
+  using tree_ptr = std::shared_ptr<const tree_type>;
+  template <class... Args> static tree_ptr make_tree(Args &&... args) {
+    return std::make_shared<tree_type>(std::forward<Args>(args)...);
+  }
+  class tree_type {
+  public:
+    T value;
+    tree_ptr left;
+    tree_ptr right;
+    tree_type(const T &value, const tree_ptr &left, const tree_ptr &right)
+        : value(value), left(left), right(right) {}
+    const T &lookup(const Size &size, const Size &index) const {
+      if (index == static_cast<Size>(0)) {
+        return value;
+      } else {
+        const Size rem_index = index - static_cast<Size>(1);
+        const Size child_size = size / static_cast<Size>(2);
+        if (rem_index < child_size) {
+          return left->lookup(child_size, rem_index);
+        } else {
+          return right->lookup(child_size, rem_index - child_size);
+        }
+      }
+    }
+    tree_ptr update(const Size &size, const Size &index,
+                    const T &new_value) const {
+      if (index == static_cast<Size>(0)) {
+        return make_tree(new_value, left, right);
+      } else {
+        const Size rem_index = index - static_cast<Size>(1);
+        const Size child_size = size / static_cast<Size>(2);
+        if (rem_index < child_size) {
+          return make_tree(
+              value, left->update(child_size, rem_index, new_value), right);
+        } else {
+          return make_tree(
+              value, left,
+              right->update(child_size, rem_index - child_size, new_value));
+        }
+      }
+    }
+  };
+  class digit_type;
+  using digit_ptr = std::shared_ptr<const digit_type>;
+  template <class... Args> static digit_ptr make_digit(Args &&... args) {
+    return std::make_shared<digit_type>(std::forward<Args>(args)...);
+  }
+  class digit_type {
+  public:
+    Size size;
+    tree_ptr tree;
+    digit_ptr next;
+    digit_type(const Size &size, const tree_ptr &tree, const digit_ptr &next)
+        : size(size), tree(tree), next(next) {}
+    const T &lookup(const Size &index) const {
+      if (index < size) {
+        return tree->lookup(size, index);
+      } else {
+        return next->lookup(index - size);
+      }
+    }
+    digit_ptr update(const Size &index, const T &new_value) const {
+      if (index < size) {
+        return make_digit(size, tree->update(size, index, new_value), next);
+      } else {
+        return make_digit(size, tree, next->update(index - size, new_value));
+      }
+    }
+  };
+
+  digit_ptr root;
+
+  explicit persistent_skew_binary_list(const digit_ptr &root) : root(root) {}
+
+public:
+  persistent_skew_binary_list() : root() {}
+
+  bool is_empty() const { return !static_cast<bool>(root); }
+  persistent_skew_binary_list cons(const T &new_value) const {
+    if (root && root->next && root->size == root->next->size) {
+      return persistent_skew_binary_list(
+          make_digit(static_cast<Size>(1) + root->size + root->next->size,
+                     make_tree(new_value, root->tree, root->next->tree),
+                     root->next->next));
+    } else {
+      return persistent_skew_binary_list(make_digit(
+          static_cast<Size>(1), make_tree(new_value, nullptr, nullptr), root));
+    }
+  }
+  const T &head() const { return root->tree->value; }
+  persistent_skew_binary_list tail() const {
+    if (root->size == static_cast<Size>(1)) {
+      return persistent_skew_binary_list(root->next);
+    } else {
+      const Size &child_size = root->size / static_cast<Size>(2);
+      return persistent_skew_binary_list(
+          make_digit(child_size, root->tree->left,
+                     make_digit(child_size, root->tree->right, root->next)));
+    }
+  }
+  const T &lookup(const Size &index) const { return root->lookup(index); }
+  persistent_skew_binary_list update(const Size &index,
+                                     const T &new_value) const {
+    return persistent_skew_binary_list(root->update(index, new_value));
+  }
+};
+
+} // namespace n91
+
+#endif
---
+++ test/data_structure/sequence/persistent_skew_binary_list.cpp
@@ -0,0 +1,98 @@
+#include <lib/data_structure/sequence/persistent_skew_binary_list>
+#include <cassert>
+#include <cstddef>
+#include <limits>
+#include <random>
+#include <string>
+#include <vector>
+
+namespace n91 {
+
+class checker {
+public:
+  std::vector<int> vec;
+  persistent_skew_binary_list<int> list;
+
+  checker() : list(), vec() {}
+  void check() const {
+    assert(list.is_empty() == vec.empty());
+    if (!vec.empty()) {
+      assert(list.head() == vec.front());
+    }
+    for (std::size_t i = 0; i != vec.size(); ++i) {
+      assert(list.lookup(i) == vec[i]);
+    }
+  }
+  checker cons(const int &new_value) const {
+    checker ret;
+    ret.list = list.cons(new_value);
+    ret.vec = vec;
+    ret.vec.insert(ret.vec.begin(), new_value);
+    return std::move(ret);
+  }
+  checker tail() const {
+    checker ret;
+    ret.list = list.tail();
+    ret.vec = vec;
+    ret.vec.erase(ret.vec.begin());
+    return std::move(ret);
+  }
+  checker update(const std::size_t index, const int &new_value) const {
+    checker ret;
+    ret.list = list.update(index, new_value);
+    ret.vec = vec;
+    ret.vec[index] = new_value;
+    return std::move(ret);
+  }
+};
+
+void test() {
+  const std::size_t q = 3000;
+  std::default_random_engine engine(91);
+  std::vector<checker> past(1);
+  for (std::size_t i = 0; i != q; ++i) {
+    std::size_t old_i = i;
+    while (old_i != 0) {
+      if (std::uniform_int_distribution<int>(0, 2)(engine) == 0) {
+        break;
+      } else {
+        --old_i;
+      }
+    }
+    const checker &old = past[old_i];
+    std::vector<std::string> ops;
+    ops.emplace_back("cons");
+    ops.emplace_back("cons");
+    ops.emplace_back("cons");
+    if (!old.vec.empty()) {
+      ops.emplace_back("tail");
+      ops.emplace_back("update");
+    }
+    const std::string &op = ops[std::uniform_int_distribution<std::size_t>(
+        0, ops.size() - 1)(engine)];
+    if (op == "cons") {
+      past.push_back(old.cons(std::uniform_int_distribution<int>(
+          std::numeric_limits<int>::lowest(),
+          std::numeric_limits<int>::max())(engine)));
+    } else if (op == "tail") {
+      past.push_back(old.tail());
+    } else if (op == "update") {
+      past.push_back(old.update(std::uniform_int_distribution<std::size_t>(
+                                    0, old.vec.size() - 1)(engine),
+                                std::uniform_int_distribution<int>(
+                                    std::numeric_limits<int>::lowest(),
+                                    std::numeric_limits<int>::max())(engine)));
+    } else {
+      assert(false);
+    }
+    past.back().check();
+  }
+  return;
+}
+
+} // namespace n91
+
+int main() {
+  n91::test();
+  return 0;
+}
